364a9fb546837b9943fedf4ba61a9032c3fd4bb5
Dan Untenzu <untenzu@webit.de>
2013-12-18 17:50:44 +0100

    [FEATURE] Backwards compatibility
    Enable task for TYPO3 CMS versions 4.5 & 4.7 as well

--- src/typo3/formhandler_clearlog_task/ext_emconf.php
+++ src/typo3/formhandler_clearlog_task/ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.1.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
--- src/typo3/formhandler_clearlog_task/ext_localconf.php
+++ src/typo3/formhandler_clearlog_task/ext_localconf.php
@@ -3,12 +3,26 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-// Register formhandler log table in table garbage collection task
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'])) {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'] = array(
-		'dateField' => 'tstamp',
-		'expirePeriod' => 180
-	);
+$version = class_exists('t3lib_utility_VersionNumber')
+	? t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version)
+	: t3lib_div::int_from_ver(TYPO3_version);
+if ($version >= 6000000) {
+	// Register formhandler log table in table garbage collection task
+	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'] = array(
+			'dateField' => 'tstamp',
+			'expirePeriod' => 180
+		);
+	}
+}
+else {
+	// …same for TYPO3 CMS < 6.0
+	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_formhandler_log'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_formhandler_log'] = array(
+			'dateField' => 'tstamp',
+			'expirePeriod' => 180
+		);
+	}
 }
 
 ?>
