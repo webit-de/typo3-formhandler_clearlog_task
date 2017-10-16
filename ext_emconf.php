7319910543845beae163aca2b1c8065201bebbd7
Dan Untenzu <untenzu@webit.de>
2017-10-16 16:13:33 +0200

    [FEATURE] Switch to PSR-2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,28 +11,26 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Formhandler ClearLog Task',
-	'description' => 'Add formhandler log table to table garbage collection task',
-	'category' => 'misc',
-	'author' => 'Dan Untenzu',
-	'author_email' => 'untenzu@webit.de',
-	'author_company' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 1,
-	'version' => '1.0.1',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.5.0-7.0.99',
-			'typo3' => '6.2.0-7.6.99',
-			'cms' => '',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Formhandler ClearLog Task',
+    'description' => 'Add formhandler log table to table garbage collection task',
+    'category' => 'misc',
+    'author' => 'Dan Untenzu',
+    'author_email' => 'untenzu@webit.de',
+    'author_company' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.5.0-7.0.99',
+            'typo3' => '6.2.0-7.6.99',
+            'cms' => '',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
-
-?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,12 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die('Access denied.');
 }
 
 // Register formhandler log table in table garbage collection task
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'])) {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'] = array(
-		'dateField' => 'tstamp',
-		'expirePeriod' => 180
-	);
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_formhandler_log'] = array(
+        'dateField' => 'tstamp',
+        'expirePeriod' => 180
+    );
 }
-
-?>
