87683cd240e2471e0df824ad4269d2ac56af56a6
Dan Untenzu <untenzu@webit.de>
2018-05-17 15:36:15 +0200

    [FEATURE] Compatibility with TYPO3 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.5.0-7.0.99',
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '6.2.0-8.7.99',
             'cms' => '',
         ),
         'conflicts' => array(
